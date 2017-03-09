baed5926adc5b909a8f1fe68600166c18a259ad2
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-09 15:02:20 +0100

    Serializer

--- src/Deinok.Runtime.Serialization/ISerializerExtension.cs
+++ src/Deinok.Runtime.Serialization/ISerializerExtension.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Deinok.Runtime.Serialization{
 
-    public static class ISerializerExtension<TInput,TOutput>{
+    public static class ISerializerExtension{
 
 		/*
-		public static TOuput SerializeAsync(this ISerializer<TInput,TOutput> serializer){
+		public static Task<TOuput> SerializeAsync(this ISerializer<TInput,TOutput> serializer){
 
 		}
 		*/
---
+++ tests/Deinok.Runtime.Serialization.Tests/ISerializerTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Deinok.Runtime.Serialization.Tests {
+
+	public class ISerializerTest{
+
+        [Fact]
+        public void SerializerTest() {
+			SerializerMock serializer = new SerializerMock();
+			Assert.Equal("", serializer.Serialize("anyThing"));
+        }
+
+		private class SerializerMock : ISerializer<string, string>{
+
+			public string Serialize(string input){
+				return "";
+			}
+
+		}
+
+    }
+
+}
