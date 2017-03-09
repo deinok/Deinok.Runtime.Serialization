2444ba3c1c4a58e6775e95fbbaeb19e9725cb5df
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-09 15:50:43 +0100

    Deserializer Test

---
+++ tests/Deinok.Runtime.Serialization.Tests/IDeserializerTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Deinok.Runtime.Serialization.Tests {
+
+	public class IDeserializerTest {
+
+		[Fact]
+		public void DeserializerDeserializeTest(){
+			IDeserializer<string, string> deserializer = new DeserializerMock();
+			Assert.Equal("", deserializer.Deserialize("anyThing"));
+		}
+
+		[Fact]
+		public async void DeserializerDeserializeAsyncTest(){
+			IDeserializer<string, string> deserializer = new DeserializerMock();
+			Assert.Equal("", await deserializer.DeserializeAsync("anyThing"));
+		}
+
+		private class DeserializerMock : IDeserializer<string, string>{
+
+			public string Deserialize(string input){
+				return "";
+			}
+
+		}
+
+	}
+
+}
