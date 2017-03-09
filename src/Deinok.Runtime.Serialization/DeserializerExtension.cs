2d76fda2cc0c4dc72f2a535d8010751be92a564f
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-09 15:47:58 +0100

    Deserializer

---
+++ src/Deinok.Runtime.Serialization/DeserializerExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Deinok.Runtime.Serialization {
+
+	/// <summary>
+	/// Extension Methods for IDeserializer
+	/// </summary>
+	public static class IDeserializerExtension {
+
+		/// <summary>
+		/// Deserializes the input async
+		/// </summary>
+		/// <typeparam name="TInput">The Input Type</typeparam>
+		/// <typeparam name="TOutput">The Output Type</typeparam>
+		/// <param name="deserializer">The Deserializer instance</param>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output Task</returns>
+		public static async Task<TOutput> DeserializeAsync<TInput, TOutput>(this IDeserializer<TInput, TOutput> deserializer, TInput input){
+			return await Task.Run<TOutput>(() => deserializer.Deserialize(input));
+		}
+
+	}
+
+}
---
+++ src/Deinok.Runtime.Serialization/IDeserializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Deinok.Runtime.Serialization {
+
+	/// <summary>
+	/// Interface to Deserialize a TInput to TOutput
+	/// </summary>
+	/// <typeparam name="TInput">The Input Type</typeparam>
+	/// <typeparam name="TOutput">The Output Type</typeparam>
+	public interface IDeserializer<TInput, TOutput> {
+
+		/// <summary>
+		/// Deerialize a TInput to a TOutput
+		/// </summary>
+		/// <param name="input">The Input Type</param>
+		/// <returns>The Output Type</returns>
+		TOutput Deserialize(TInput input);
+
+	}
+
+}
