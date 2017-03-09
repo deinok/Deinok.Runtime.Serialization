108b2f2c8cf030956426fa21a0a901b9e0f8c8c3
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-09 15:22:40 +0100

    XML Documents

--- src/Deinok.Runtime.Serialization/ISerializerExtension.cs
+++ src/Deinok.Runtime.Serialization/ISerializerExtension.cs
@@ -2,8 +2,19 @@
 
 namespace Deinok.Runtime.Serialization{
 
+	/// <summary>
+	/// Extension Methods for ISerializer
+	/// </summary>
     public static class ISerializerExtension{
 
+		/// <summary>
+		/// Serializes the input async
+		/// </summary>
+		/// <typeparam name="TInput">The Input Type</typeparam>
+		/// <typeparam name="TOutput">The Output Type</typeparam>
+		/// <param name="serializer">The Serializer instance</param>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output Task</returns>
 		public static async Task<TOutput> SerializeAsync<TInput,TOutput>(this ISerializer<TInput,TOutput> serializer, TInput input){
 			return await Task.Run<TOutput>(()=> serializer.Serialize(input));
 		}
