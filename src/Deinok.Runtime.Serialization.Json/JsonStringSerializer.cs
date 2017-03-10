7d5667c6404035648b0d4cc5dc29664fa1ef8f02
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-10 17:32:44 +0100

    BEtter XML

--- src/Deinok.Runtime.Serialization.Json/JsonStringSerializer.cs
+++ src/Deinok.Runtime.Serialization.Json/JsonStringSerializer.cs
@@ -3,13 +3,27 @@ using Newtonsoft.Json;
 
 namespace Deinok.Runtime.Serialization.Json {
 
+	/// <summary>
+	/// Serializes objects to a Json String 
+	/// </summary>
 	public class JsonStringSerializer:BaseGenericSerializer<string> {
 		
+		/// <summary>
+		/// Serialize a object
+		/// </summary>
+		/// <typeparam name="TInput">The Deserialized Type</typeparam>
+		/// <param name="input">The Input</param>
+		/// <returns>The Json String</returns>
 		public override string Serialize<TInput>(TInput input){
 			return JsonConvert.SerializeObject(input);
 		}
 
-
+		/// <summary>
+		/// Deserialize a Json String
+		/// </summary>
+		/// <typeparam name="TOutput">The Deserialized Type</typeparam>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output</returns>
 		public override TOutput Deserialize<TOutput>(string input){
 			return JsonConvert.DeserializeObject<TOutput>(input);
 		}
