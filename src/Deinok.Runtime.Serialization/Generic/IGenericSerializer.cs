a0cafd325a0b6ee460af2ac515c633855484bc2a
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-10 16:46:30 +0100

    IGenericSerializer

---
+++ src/Deinok.Runtime.Serialization/Generic/IGenericSerializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Deinok.Runtime.Serialization.Generic {
+
+	/// <summary>
+	/// A Generic Serializer
+	/// </summary>
+	/// <typeparam name="TOutput">The Output Type</typeparam>
+	public interface IGenericSerializer<TOutput> {
+
+		/// <summary>
+		/// Serializes a TInput to TOutput
+		/// </summary>
+		/// <typeparam name="TInput">The Input Type</typeparam>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output</returns>
+		TOutput Serialize<TInput>(TInput input);
+
+	}
+
+}
