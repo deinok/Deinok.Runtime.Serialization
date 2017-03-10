40a574e49ce89223ac9d4d651a649bef7d854548
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-10 16:55:58 +0100

    XmlDoc

--- src/Deinok.Runtime.Serialization/Generic/BaseGenericSerializer.cs
+++ src/Deinok.Runtime.Serialization/Generic/BaseGenericSerializer.cs
@@ -1,10 +1,25 @@
 ﻿namespace Deinok.Runtime.Serialization.Generic {
 
+	/// <summary>
+	/// Base Generic Serializer for Everything
+	/// </summary>
+	/// <typeparam name="TOutput">The Output Type of the Serialization</typeparam>
 	public abstract class BaseGenericSerializer<TOutput> : IGenericSerializer<TOutput>,IGenericDeserializer<TOutput> {
 
-
+		/// <summary>
+		/// Serialize a TInput to a TOutput
+		/// </summary>
+		/// <typeparam name="TInput">The Input Type</typeparam>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output</returns>
 		public abstract TOutput Serialize<TInput>(TInput input);
 
+		/// <summary>
+		/// Deserialize a TOutput to a TInput
+		/// </summary>
+		/// <typeparam name="TInput">The Input Type</typeparam>
+		/// <param name="input">The Input</param>
+		/// <returns>The Output</returns>
 		public abstract TInput Deserialize<TInput>(TOutput input);
 
 	}
