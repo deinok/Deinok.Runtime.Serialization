ca870eb196ebced8219c88070b8187ac5eba70bf
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-03-10 16:52:27 +0100

    BaseGenericSerializer

---
+++ src/Deinok.Runtime.Serialization/Generic/BaseGenericSerializer.cs
@@ -0,0 +1,12 @@
+﻿namespace Deinok.Runtime.Serialization.Generic {
+
+	public abstract class BaseGenericSerializer<TOutput> : IGenericSerializer<TOutput>,IGenericDeserializer<TOutput> {
+
+
+		public abstract TOutput Serialize<TInput>(TInput input);
+
+		public abstract TInput Deserialize<TInput>(TOutput input);
+
+	}
+
+}
